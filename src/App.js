9700167f9295d30f237e6874f3e87bd88a950797
Brav0 <rybinil1998@gmail.com>
2022-10-19 21:37:16 +0300

    complited app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Home } from './pages/Home';
+import { Category } from './pages/Category';
+import { Recipe } from './pages/Recipe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router basename='/food-app'>
+        <Header />
+        <main className='container content'>
+          <Routes>
+            <Route exect path='/' element={<Home />} />
+            <Route path='/category/:name' element={<Category />} />
+            <Route path='/meal/:id' element={<Recipe />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
